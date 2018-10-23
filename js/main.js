2a0ac7b372f7311e58a098788431b83b7a514d15
Sam Ng <chanhnp@gmail.com>
2018-10-23 11:41:56 -0700

    cleaned up code

--- js/main.js
+++ js/main.js
@@ -1,15 +1,11 @@
-(() => {
-    // var a = new StickySidebar('#sidebar', {
-    //     topSpacing: 20,
-    //     bottomSpacing: 20,
-    //     containerSelector: '.container',
-    //     innerWrapperSelector: '.sidebar__inner'
-    // });
+// Author: Sam Ng - chanhnp@gmail.com, github.com/exfoxz
 
+(() => {
     window.addEventListener("load", function(event) {
         console.log("All resources finished loading!");
+
+        var sidebarEl = document.querySelector("#sidebar .inner");
         var expandBtn = document.getElementById("expand-sidebar");
-        console.log(expandBtn);
 
         expandBtn.onclick = (e) => {
             let cl = document.querySelector("#right").classList;
@@ -22,12 +18,6 @@
                 expandBtn.innerHTML = "Close";
             }
         }
-
-        // document.getElementById("content").onclick = () => {
-        //  console.log("AKSJDKASJD");
-        //  document.querySelector(".sidebar__inner").classList.remove("popout");
-        // }
-
 
         // register event handlers to all spans
         var spans = document.getElementsByTagName('span');
@@ -48,38 +38,31 @@
         }
 
         function loadTemplate(e) {
-            console.log('loading template')
             var templatePath = e.target.dataset["template"];
             console.log(templatePath);
 
             fetch(templatePath)
             .then(response => response.text())
             .then(text => {
-                document.querySelector(".sidebar__inner .inner").classList.add('pre-animation');
+                sidebarEl.classList.add('pre-animation');
 
                 setTimeout(function(){
-                    document.querySelector(".sidebar__inner .inner").innerHTML = text; 
-                    document.querySelector(".sidebar__inner .inner").classList.remove('pre-animation')
+                    sidebarEl.innerHTML = text; 
+                    sidebarEl.classList.remove('pre-animation')
                 }, 200)
-
-                console.log(text)
             })
+        }
 
-              // outputs the content of the text file
-          }
-
-          function doSomething(e) {
-            console.log('yess')
+        function doSomething(e) {
             var content = e.target.dataset["text"];
-            console.log(content);
 
             if(content) {
-                document.querySelector(".sidebar__inner .inner").classList.add('pre-animation');
+                sidebarEl.classList.add('pre-animation');
 
 
                 setTimeout(function(){
-                    document.querySelector(".sidebar__inner .inner").innerHTML = "<p>" + content + "</p>";
-                    document.querySelector(".sidebar__inner .inner").classList.remove('pre-animation')
+                    sidebarEl.innerHTML = "<p>" + content + "</p>";
+                    sidebarEl.classList.remove('pre-animation')
                 }, 200)
             }
         }
